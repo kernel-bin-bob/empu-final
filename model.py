ee20d89b1b5a167112d7b4e1e82d82fee63fbfe3
Borys <borys@MacBook-Pro-Borys.local>
2026-07-19 12:22:21 +0300

    Cleanup

--- model.py
+++ model.py
@@ -9,70 +9,59 @@ from joblib import dump
 
 
 def train_regression_model(X_train: ArrayLike, y_train: ArrayLike) -> LinearRegression:
-    """
-    Train a regression model using the provided training data.
+    # Train a regression model using the provided training data.
 
-    This function takes training data consisting of a feature matrix 'X_train' and
-    corresponding target values 'y_train', and trains a linear regression model.
-    The trained model is returned for further use.
+    # This function takes training data consisting of a feature matrix 'X_train' and
+    # corresponding target values 'y_train', and trains a linear regression model.
+    # The trained model is returned for further use.
 
-    Args:
-        X_train (array-like): Training feature matrix.
-        y_train (array-like): Target values for training.
+    # Args:
+    #     X_train (array-like): Training feature matrix.
+    #     y_train (array-like): Target values for training.
 
-    Returns:
-        sklearn.linear_model.LinearRegression: Trained linear regression model.
-
-    """
+    # Returns:
+    #     sklearn.linear_model.LinearRegression: Trained linear regression model.
 
     return LinearRegression().fit(X_train, y_train)
 
 def save_regression_model(model: LinearRegression, filename: str = "linear_regression_model.joblib"):
-    """
-    Serialize and save the regression model.
+    # Serialize and save the regression model.
 
-    This function takes a trained regression 'model' and file name 'filename' that has a default value.
-    It uses Joblib 'dump' to save the model using the provided file name.
+    # This function takes a trained regression 'model' and file name 'filename' that has a default value.
+    # It uses Joblib 'dump' to save the model using the provided file name.
 
-    Args:
-        model (sklearn.linear_model.LinearRegression): Trained regression model to be evaluated.
-        filename (str): Name of the file that is used to store the model.
+    # Args:
+    #     model (sklearn.linear_model.LinearRegression): Trained regression model to be evaluated.
+    #     filename (str): Name of the file that is used to store the model.
 
-    """
-    
     dump(model, filename)
 
 def evaluate_regression_model(model: LinearRegression, X_test: ArrayLike, y_test: ArrayLike):
-    """
-    Evaluate the performance of a regression model on test data.
+    # Evaluate the performance of a regression model on test data.
 
-    This function takes a trained regression 'model', test feature matrix 'X_test',
-    and corresponding test target values 'y_test'. It calculates Mean Squared Error (MSE)
-    and prints it in terminal.
+    # This function takes a trained regression 'model', test feature matrix 'X_test',
+    # and corresponding test target values 'y_test'. It calculates Mean Squared Error (MSE)
+    # and prints it in terminal.
 
-    Args:
-        model (sklearn.linear_model.LinearRegression): Trained regression model to be evaluated.
-        X_test (array-like): Test feature matrix.
-        y_test (array-like): Validation target values.
-
-    """
+    # Args:
+    #     model (sklearn.linear_model.LinearRegression): Trained regression model to be evaluated.
+    #     X_test (array-like): Test feature matrix.
+    #     y_test (array-like): Validation target values.
     
     y_predicted = model.predict(X_test)
     mse = mean_squared_error(y_test, y_predicted)
     print(f"Mean Squared Error: {mse}")
 
 def save_initial_datasets(X: ArrayLike, y: ArrayLike):
-    """
-    Serialize and save datasets.
+    # Serialize and save datasets.
 
-    This function takes entire feature matrix 'X', and corresponding target values 'y'.
-    It uses Joblib 'dump' to save both arrays as predefined files.
+    # This function takes entire feature matrix 'X', and corresponding target values 'y'.
+    # It uses Joblib 'dump' to save both arrays as predefined files.
 
-    Args:
-        X (array-like): Test feature matrix.
-        y (array-like): Validation target values.
-
-    """
+    # Args:
+    #     X (array-like): Test feature matrix.
+    #     y (array-like): Validation target values.
+    
     X_filename = "X.joblib"
     y_filename = "y.joblib"
     
@@ -80,23 +69,15 @@ def save_initial_datasets(X: ArrayLike, y: ArrayLike):
     dump(y, y_filename)
 
 if __name__ == '__main__':
-    # Generate a dataset
+    # Create dataset
     X, y = make_regression(n_samples=100, n_features=1, noise=20, random_state=42)
-
-    # Scale feature X to range -3..3
     X = np.interp(X, (X.min(), X.max()), (-3, 3))
 
-    # Split the dataset into training and test sets
+    # Train and evaluate the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Train a linear regression model
     model = train_regression_model(X_train, y_train)
-
-    # Evaluate the model
     evaluate_regression_model(model, X_test, y_test)
 
-    # Save the model
+    # Save model and datasets
     save_regression_model(model)
-
-    # Save datasets
     save_initial_datasets(X, y)
